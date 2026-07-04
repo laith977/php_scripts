a3206eecfc54f561b10d0feab38b33d354e54f9e
Laith Adnan <lith.adnan@yahoo.com>
2026-07-04 13:37:23 +0300

    change

--- FactoryMethod/Factory/notification_factory.php
+++ FactoryMethod/Factory/notification_factory.php
@@ -5,4 +5,10 @@ require_once __DIR__ . '/../Product/notification.php';
 abstract class NotificationFactory
 {
     abstract public function create(): Notification;
+
+    public function notify(string $message): void
+    {
+        $notification = $this->create();
+        $notification->send($message);
+    }
 }
